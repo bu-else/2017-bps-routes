80a901896e9802326f93aa1ae91cd562a6f33bf6
Zui <zuikng@gmail.com>
2017-11-16 14:14:29 -0500

    If we gonna do GA, we should put it in a seperate file

--- scripts/upload.js
+++ scripts/upload.js
@@ -165,12 +165,3 @@ var do_file = (function() {
 	function handleFile(e) { do_file(e.target.files); }
 	xlf.addEventListener('change', handleFile, false);
 })();
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-36810333-1']);
-	_gaq.push(['_trackPageview']);
-
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();

e90e7bf9d52e4f0a13e92ef5703552ac5f6e6681
Duy Nguyen <zuikng@gmail.com>
2017-11-27 20:32:15 -0500

    Test route

---
+++ tools/xlsx2json.js
@@ -0,0 +1,18 @@
+/**
+ * Tool to convert a Excel .xlsx spreadsheet to .json
+ */
+
+const XLSX = require('xlsx');
+
+const ARGV = process.argv.slice(2);
+
+
+if (ARGV.length() > 0) {
+    let inputFile = ARGV[0];
+
+}
+
+
+function parseFile (path) {
+    
+}

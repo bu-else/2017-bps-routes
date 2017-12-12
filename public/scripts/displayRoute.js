3044dfe72068b476fe193424e366442f688a9ef8
Jessica McAloon <mcaloonj@bu.edu>
2017-12-12 15:35:44 -0500

    update displayRoute

--- public/scripts/displayRoute.js
+++ public/scripts/displayRoute.js
@@ -9,7 +9,7 @@ var routeMap = {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox.streets',
-            accessToken: 'ACCESS TOKEN'
+            accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
         }).addTo(this.map)
     },
     _displayBusRoute: function(latlongs, id, color) {
